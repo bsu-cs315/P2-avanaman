1af8f5bcc48b8eb900cb3d5167366ed9e65ae8c8
Ashley <avanaman@bsu.edu>
2022-09-27 12:37:39 -0400

    added player code

--- Player.gd
+++ Player.gd
@@ -1,14 +1,30 @@
 extends KinematicBody2D
 
+const _GRAVITY := 100
+const _SPEED := 100
+const _JUMP := 200
 
+var _velocity := Vector2.ZERO
+
+onready var animation := get_node('Green')
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
-func _process(delta:float):
-	pass # Replace with function body.
+func _physics_process(delta:float)->void:
+	_velocity.y = 0.0 if is_on_floor() else _velocity.y + _GRAVITY * delta
+	var input := Vector2.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		_velocity.y = - _JUMP
+		
+	_velocity.x = input.x * _SPEED
+	
+	move_and_slide(_velocity, Vector2.UP)
+	# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
